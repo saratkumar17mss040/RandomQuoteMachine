79123651b09df2458371789100d46c3987c5934c
sarath <sarathdon9245@gmail.com>
2020-09-15 19:13:03 +0530

    🚀 finished

--- src/app.js
+++ src/app.js
@@ -1,23 +1,52 @@
+// selection of necessary elements for DOM manipluation
 const quote = document.getElementById('text');
 const author = document.getElementById('author');
+const twitterButton = document.getElementById('btn-twitter');
+const tumblrButton = document.getElementById('btn-tumblr');
+const body = document.querySelector('body');
+const quoteBox = document.getElementById('quote-box');
 const quoteButton = document.getElementById('new-quote');
+const icons = document.getElementsByClassName('fa');
 quoteButton.addEventListener('click', generateRandomQuote);
+// generate random quote as soon as the window is loaded with all the assets
 window.onload = generateRandomQuote;
 
+// generate random quote function
 function generateRandomQuote() {
-    let request = new XMLHttpRequest();
-    request.open('GET', 'http://quotes.stormconsultancy.co.uk/random.json', true);
+	let request = new XMLHttpRequest();
+	request.open('GET', 'http://quotes.stormconsultancy.co.uk/random.json', true);
 
-    request.onload = function()  {
-        if (this.status === 200) {
-            var quoteObject = JSON.parse(this.responseText);
-            quote.innerHTML = quoteObject.quote;
-            author.innerHTML = quoteObject.author;
-        }
-    }
-    request.send();
+	request.onload = function () {
+		if (this.status === 200) {
+			var quoteObject = JSON.parse(this.responseText);
+			quote.innerHTML = quoteObject.quote;
+			author.innerHTML = quoteObject.author;
+		}
+	};
+	request.send();
+	const currentColour = generateRandomColour();
+	setColour(currentColour);
 }
 
+// generate random colour function
+const generateRandomColour = () => {
+	let letters = '0123456789ABCDEF';
+	let color = '#';
+	for (let i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)];
+	}
+	return color;
+};
 
-
-
+// setColour function
+const setColour = (currentColour) => {
+	quote.style.color = currentColour;
+	author.style.color = currentColour;
+	twitterButton.style.backgroundColor = currentColour;
+	tumblrButton.style.backgroundColor = currentColour;
+	quoteBox.style.backgroundColor = currentColour;
+	body.style.backgroundColor = currentColour;
+	for (let i = 0; i < icons.length; i++) {
+		icons[i].style.color = currentColour;
+	}
+};
--- todo.md
+++ todo.md
@@ -1,12 +1,13 @@
 ## TODO'S
 
 - [x] Create a simple wireframe using figma
-- [ ] Create a responsive page
+- [x] Create a responsive page
 - [x] Use an api to get some random quotes - storm consultancy api
 - [x] Use pure CSS
-- [ ] Follow one naming convention - camelCase
-- [ ] Keep the code consistent and comment on main actions and flows
+- [x] Follow one naming convention - camelCase
+- [x] Keep the code consistent and comment on main actions and flows
 - [x] Make a card where the quotes and the author are displayed
 - [x] Create a button to generate some random quotes
-- [ ] Change the colors of the text and the background to some random colors every time we generate a new quote
+- [x] Change the colors of the text and the background to some random colors every time we generate a new quote
 - [x] Set favicon 
+- [ ] Deploy it through surge
